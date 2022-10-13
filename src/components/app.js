1a0bec208d078cc564df3bd8a524295b1a4d25fa
SallyMulupi <mulupisally2gmail.com>
2022-10-13 23:47:38 +0300

    add add function

--- src/components/app.js
+++ src/components/app.js
@@ -8,4 +8,17 @@ function App() {
       localStorage.notes ? JSON.parse(localStorage.notes) : []
     );
     const [activeNote, setActiveNote] = useState(false);
-  
+    useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes));
+      }, [notes]);
+      const onAddNote = () => {
+        const newNote = {
+          id: uuid(),
+          title: "Untitled Note",
+          body: "",
+          lastModified: Date.now(),
+        };
+    
+        setNotes([newNote, ...notes]);
+        setActiveNote(newNote.id);
+      };
